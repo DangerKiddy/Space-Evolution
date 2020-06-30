164105bc6c4f56ca3262d7e41be771ae8886b557
DangerKiddy <42438297+DangerKiddy@users.noreply.github.com>
2020-06-30 22:01:42 +0700

    Add files via upload

---
+++ objects.lua
@@ -0,0 +1,9 @@
+SpaceEvo.Objects = SpaceEvo.Objects or {}
+
+function SpaceEvo.Objects:Print(msg)
+	MsgC(Color(69, 135, 255), "[Objects] ", color_white, msg.."\n")
+end
+
+function SpaceEvo.Objects:Create(objName, objPixel)
+	
+end
