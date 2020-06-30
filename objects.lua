9ef54dae67f90fc39b00c6f5df9117eaca1178ac
DangerKiddy <42438297+DangerKiddy@users.noreply.github.com>
2020-06-30 22:02:00 +0700

    Delete objects.lua

--- objects.lua
+++
@@ -1,9 +0,0 @@
-SpaceEvo.Objects = SpaceEvo.Objects or {}
-
-function SpaceEvo.Objects:Print(msg)
-	MsgC(Color(69, 135, 255), "[Objects] ", color_white, msg.."\n")
-end
-
-function SpaceEvo.Objects:Create(objName, objPixel)
-	
-end
